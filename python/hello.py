3f439c723c38a25d50dcb8820cc09d835787e30b
whdudtjr <rtr894@naver.com>
2022-03-21 18:10:48 +0900

    backjoon

---
+++ python/hello.py
@@ -0,0 +1,38 @@
+# a = 1
+# b = 2
+# print('a와b 의 합:',a+b)
+
+# a= 1
+# b= 1.2
+# c= 'String'
+# print('정수 출력: ',a, "변수 타입", type(a))
+# print('실수 출력: ',b, "변수 타입", type(b))
+# print('문자열 출력: ',c, "변수 타입", type(c))
+
+# 3번 
+# print("\    /\ ")
+# print(" )  ( ')")
+# print("(  /  )")
+# print(" \(__)|")  
+        
+# \    /\
+#  )  ( ')
+# (  /  )
+#  \(__)|
+
+#4번
+
+# |\_/|
+# |q p|   /}
+# ( 0 )"""\
+# |"^"`    |
+# ||_/=\\__|
+
+a,b = input().split()
+a = int(a)
+b = int(b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b))
+print(a%b)
